e289a2d59af45ffb4412c2fb7db3c776bc89d49e
ivodesouzabarbosa <ivosouzabarbosa@gmail.com>
2023-04-14 17:21:29 -0300

    Mudança de nome

--- Assets/Script/Person.cs
+++ Assets/Script/Person.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Person : MonoBehaviour
 {
-    string _nome = "Mario";
+    string _nome = "Mario ";
     // Start is called before the first frame update
     void Start()
     {
